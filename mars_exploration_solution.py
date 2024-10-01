2f41f2f2729ac915d7fa5f6c785b6b9ac9d06509
Haknozer <137529910+Haknozer@users.noreply.github.com>
2024-10-01 13:27:17 +0300

    mars_exploration_solution.py
    HackerRank With Python

---
+++ mars_exploration_solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    counter = 0
+    sos = "SOS" 
+    for x in range(len(s)):
+        if s[x] != sos[x%3]:
+            counter += 1
+            
+    return counter
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
